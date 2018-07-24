e85bc0e4bd2c3687b7d380cd970f3c0f30015098
Roneitis <tsawy6@gmail.com>
2018-07-24 19:51:41 +1000

    added parabaloids!#

--- src/main/java/movement/Shapes/OutlineShape.java
+++ src/main/java/movement/Shapes/OutlineShape.java
@@ -8,7 +8,6 @@ import movement.Vectors.Vector.MalformedVectorException;
 public interface OutlineShape {
 	final static int collisionDetectionGranularity = 10;
 	
-	public double getCoDmodifier();							//impacts CoD. The shape of the object has an effect!
 	public Vector getNormal(float[] position) throws MalformedVectorException; //normal of the shape at a point.
 	public double[] getDimensions();
 	public void setDimensions(double[] dimensions);
