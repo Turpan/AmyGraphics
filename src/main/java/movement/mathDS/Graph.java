9ce2fc6b1c2e482bb450714c55b57aeeca16d98d
Roneitis <tsawy6@gmail.com>
2018-11-04 22:32:01 +1000

    theree!

--- src/main/java/movement/mathDS/Graph.java
+++ src/main/java/movement/mathDS/Graph.java
@@ -2,10 +2,11 @@ package movement.mathDS;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Stack;
 
-public class Graph<T, U> {	//simple linkedList implementation of a directed graph, (Now with super hacked together edge values!!!)
+public class Graph<T, U> implements Cloneable{	//simple linkedList implementation of a directed graph, (Now with super hacked together edge values!!!)
 	private ArrayList<LinkedList<T>> adjList;	//adjacenyList. A graph concept. A list of linked lists. Each vertex is the head of a linked list, and all the vertices it's connected to are also listed in the linked list.
 	private ArrayList<Edge> edgeList;
 	public Graph() {
@@ -33,8 +34,9 @@ public class Graph<T, U> {	//simple linkedList implementation of a directed grap
 		for (LinkedList<T> LL : getAdjList()) {
 			if (LL.peek() == vertex) {
 				LLToRemove = LL;
+			}else {
+			removeEdge(LL.peek(), vertex);
 			}
-			LL.remove(vertex);
 		}
 		getAdjList().remove(LLToRemove);
 	}
@@ -53,13 +55,30 @@ public class Graph<T, U> {	//simple linkedList implementation of a directed grap
 		}
 		return null;
 	}
-	public void addEdge(T from, T to, U value) {
-		for (LinkedList<T> LL : getAdjList()) {
-			if (LL.peek() == from) {
-				LL.add(to);
-			}
-		getEdgeList().add(new Edge(from, to, value));
+	public ArrayList<T> getVertexConnections(T vertex){
+		ArrayList<T> output = new ArrayList<T>();
+		var iter = getVertexLinkedList(vertex).listIterator(1);
+		while(iter.hasNext()){
+			output.add(iter.next());
 		}
+		return output;
+	}
+	public ArrayList<T> getVerticesConnectedTo(T vertex){
+		ArrayList<T> output= new ArrayList<T>();
+		for (Edge contact :getEdgeList()) {
+			if (contact.getDest() == vertex) {
+				output.add(contact.getSource());
+			}
+		}
+		return output;
+	}
+	
+	public void addEdge(T from, T to, U value) {
+		getVertexLinkedList(from).add(to);
+		getEdgeList().add(new Edge(from, to, value));
+	}
+	public void addEdge(T from, T to) {
+		addEdge(from, to, null);
 	}
 	public void removeEdge(T from, T to) {
 		for (LinkedList<T> LL : getAdjList()) {
@@ -78,6 +97,17 @@ public class Graph<T, U> {	//simple linkedList implementation of a directed grap
 	private void setEdgeList(ArrayList<Edge> edgeList) {
 		this.edgeList = edgeList;
 	}
+	@Override
+	public Graph<T,U> clone() {
+		var output = new Graph<T, U>();
+		for (LinkedList<T> LL : getAdjList()) {
+			output.addVertex(LL.peek());
+		}for (Edge edge : getEdgeList()) {
+			output.addEdge(edge.getSource(), edge.getDest(), edge.getValue());
+		}
+		return output;
+	}
+	
 	public void prune() {//Remove Leaves! :D ALso, removes any vertices that become leaves when the leaves are removed, etc.
 		boolean hasLeaf = true;
 		ArrayList<T> leaves = new ArrayList<T>();
@@ -91,15 +121,14 @@ public class Graph<T, U> {	//simple linkedList implementation of a directed grap
 				}
 				
 			}
-			for (T leaf : leaves) {removeVertex(leaf);}
+			for (T leaf : leaves) {
+				removeVertex(leaf);
+			}
+			leaves.clear();
 		}
 	}
 	public boolean isAcyclic() {
-		var testGraph = new Graph<T, U>();
-		testGraph.setAdjList(new ArrayList<LinkedList<T>>(getAdjList()));
-		for (int i = 0; i<testGraph.getAdjList().size(); i++) {
-			testGraph.getAdjList().set(i, new LinkedList<T>(getAdjList().get(i)));
-		}
+		var testGraph = clone();
 		boolean hasLeaf = true;
 		ArrayList<T> leaves = new ArrayList<T>();
 		while (testGraph.getAdjList().size() != 0 && hasLeaf == true) {
@@ -112,7 +141,9 @@ public class Graph<T, U> {	//simple linkedList implementation of a directed grap
 				}
 				
 			}
-			for (T leaf : leaves) {removeVertex(leaf);}
+			for (T leaf : leaves) {
+				testGraph.removeVertex(leaf);
+			}
 			leaves.clear();
 			
 		}
@@ -160,7 +191,7 @@ public class Graph<T, U> {	//simple linkedList implementation of a directed grap
         
     }
  
-    // The function to do Topological Sort. It uses recursive topologicalSortUtil()
+    // The function to do Topological Sort. It uses recursive getSortedVerticesUtil()
     public ArrayList<T> getSortedVertices(){		//returns sorted list of vertices, where vertex A > B if A has an edge to B, returns null if cyclic
     	ArrayList<T> output = null;					//Code a refactord version of the algorithm for topological sorting on GeeksforGeeks.org! All credit goes to them!
     	if (isAcyclic()) {
@@ -174,36 +205,37 @@ public class Graph<T, U> {	//simple linkedList implementation of a directed grap
 	                getSortedVerticesUtil(getAdjList().get(i).peek(), visited, stack);
     		}
 	        output = new ArrayList<T>(stack);
+//	        Collections.reverse(output);	reverses the direction. I dunno if I might want to change the direction of this later.... I'm just gonna leave it commented out.
     	}
     	return output;
 	}
-    public void removeCycles() { 		//forces the graph to become acyclic. For vertices A, B if Ǝ a path from A to B before this method,
-    	var testGraph = new Graph<T, U>();	//I /try/ to maintain a path after the method (for distinct A,B), but sometimes, like in a simple ring, such a path
-		testGraph.setAdjList(new ArrayList<LinkedList<T>>(getAdjList()));	//is inherently cyclical, amd the path is destroyed, rather than leaving in a ring
-		for (int i = 0; i<testGraph.getAdjList().size(); i++) {
-			testGraph.getAdjList().set(i, new LinkedList<T>(getAdjList().get(i)));
-		}
+    public void removeCycles() { 	
+    //forces the graph to become acyclic. For vertices A, B if Ǝ a path from A to B before this method,
+    //I /try/ to maintain a path after the method (for distinct A,B), but sometimes, like in a simple ring, such a path
+	//is inherently cyclical, amd the path is destroyed, rather than leaving in a ring
     	if (!isAcyclic()) {
+			var testGraph = clone();
 			testGraph.prune();
-		}
-    	T tmp1;
-    	T tmp2;
-    	U tmp12Value;
-    	int i;
-    	while (testGraph.getAdjList().size() > 1) {
-    		i = 1;
-    		tmp1 = testGraph.getAdjList().get(0).peek();
-    		tmp2 = testGraph.getAdjList().get(0).get(i);
-    		tmp12Value = testGraph.getEdgeValue(tmp1, tmp2);
-			testGraph.removeEdge(tmp1, tmp2);	//remove an arbitrary edge. This one is guaranteed to exist, so long as the graph is pruned & size > 1;
-			if (!testGraph.isConnected(tmp1, tmp2) && i + 1 < testGraph.getAdjList().get(0).size()){
-				testGraph.addEdge(tmp1, tmp2, tmp12Value);	//sometimes removing an arbitrary edge will break paths. I can't let that happen, so this. It's inefficient, but I frankly don't know enough about graphs to know if there was an analytic solution .
-				i++;
-			} else {
-				removeEdge(tmp1, tmp2);
-				testGraph.prune();
-			}
+	    	T tmp1;
+	    	T tmp2;
+	    	U tmp12Value;
+	    	int i;
+	    	while (testGraph.getAdjList().size() > 1 && !testGraph.isAcyclic()) {
+	    		i = 1;
+	    		tmp1 = testGraph.getAdjList().get(0).peek();
+	    		tmp2 = testGraph.getAdjList().get(0).get(i);
+	    		tmp12Value = testGraph.getEdgeValue(tmp2, tmp1);
+				testGraph.removeEdge(tmp1, tmp2);	//remove an arbitrary edge. This one is guaranteed to exist, so long as the graph is pruned & size > 1;
+				if (!testGraph.isConnected(tmp1, tmp2) && i + 1 < testGraph.getAdjList().get(0).size()){
+					testGraph.addEdge(tmp1, tmp2, tmp12Value);	//sometimes removing an arbitrary edge will break paths. I can't let that happen, so this. It's inefficient, but I frankly don't know enough about graphs to know if there was an analytic solution .
+					i++;
+				} else {
+					removeEdge(tmp1, tmp2);
+					testGraph.prune();
+				}
+	    	}
     	}
+    	
     }
     public String toString() {
     	var output = "";
@@ -217,9 +249,9 @@ public class Graph<T, U> {	//simple linkedList implementation of a directed grap
     		output += tmp.substring(0,tmp.length()-2) + "... ";
     	}
     	return output;
-   }
-	
-private class Edge{
+    }
+
+	private class Edge{
     	@Override
 		public int hashCode() {
 			final int prime = 31;
@@ -238,6 +270,7 @@ private class Edge{
 				return false;
 			if (getClass() != obj.getClass())
 				return false;
+			@SuppressWarnings("unchecked")
 			Edge other = (Edge) obj;
 			if (!getOuterType().equals(other.getOuterType()))
 				return false;
@@ -290,7 +323,7 @@ private class Edge{
 		public void setValue(U value) {
 			this.value = value;
 		}
-		private Graph getOuterType() {
+		private Graph<T, U> getOuterType() {
 			return Graph.this;
 		}
     }
