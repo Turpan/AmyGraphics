dd4fd5688fbdc6bbe9c36b84f999f8f72b903cb1
Roneitis <tsawy6@gmail.com>
2019-05-06 22:31:56 +1000

    bugs n shit

--- src/main/java/movement/SelfPropelled.java
+++ src/main/java/movement/SelfPropelled.java
@@ -3,6 +3,12 @@ package movement;
 import movement.mathDS.Vector;
 
 public abstract class SelfPropelled extends Movable {
+	//SelfPropelleds can move themselves. They can generate a force in a direction, if they so
+	//please. They require a baseMoveForce.
+	
+	//Examples of SelfPropelleds that don't inherit this classes children include:
+	//simple creatures that move in complex ways, but aren't fully fleshed characters. Say, 
+	//a wisp that follows you.
 	private double baseMoveForce;
 
 	public SelfPropelled() {
